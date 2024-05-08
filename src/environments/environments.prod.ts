98cb36ae59ec001626e9be7c2d8d18b24107937b
eduarchiloncruz <eduarchilon@outlook.es>
2024-05-08 19:51:38 -0300

    fix: change environments library to ngx-env

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,5 @@
+export const environments = {
+  production: true,
+  baseUrl: import.meta.env['NG_APP_BASE_URL'],
+  email: import.meta.env['NG_APP_EMAIL_USER'],
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environments = {
+  baseUrl: import.meta.env['NG_APP_BASE_URL'],
+  email: import.meta.env['NG_APP_EMAIL_USER'],
+};
